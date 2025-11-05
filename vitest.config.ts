7abfee1c90e4c36869435e736caa88ea7ad70714
Somraj Saha <31373860+Jarmos-san@users.noreply.github.com>
2025-11-05 15:39:36 +0530

    chore: update the Vitest configurations

--- vitest.config.ts
+++ vitest.config.ts
@@ -9,5 +9,19 @@ export default defineConfig({
       enabled: true,
       provider: "v8",
     },
+    silent: "passed-only",
+    sequence: {
+      concurrent: true,
+    },
+    typecheck: {
+      enabled: process.env.CI ? true : false,
+    },
+    bail: process.env.CI ? 3 : undefined,
+    retry: process.env.CI ? 3 : 0,
+    printConsoleTrace: process.env.CI ? true : false,
+    reporters: process.env.GITHUB_ACTIONS
+      ? ["dot", "github-actions"]
+      : "default",
   },
+  cacheDir: process.env.VITEST ? "./.vitest" : undefined,
 });
