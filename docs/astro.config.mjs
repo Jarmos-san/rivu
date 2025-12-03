027e406aef80fd5b3d33087e3b9c35e4ad0df5d5
Somraj Saha <31373860+Jarmos-san@users.noreply.github.com>
2025-12-03 17:22:54 +0530

    chore: add Starlight for the docs (#24)
    ## Description
    
    This PR involves setting up the documentation for the project. A full
    list of all changes introduced with the PR are as follows:
    
    1. Added a Dependabot config file to ensure the dependencies of the
    documentation are updated automatically.
    2. Added and configured a Starlight theme. The theme has also been
    slightly configured to reflect the information of the project.
    3. Updated the `.editorconfig` file to recognise the source code of the
    documentation.
    
    ## Related Issue / Ticket
    
    Related to #23 
    
    ## Type of Change
    
    Select the type of change this PR introduces:
    
    - [ ] Feature
    - [ ] Bug Fix
    - [ ] Refactor
    - [x] Documentation Update
    - [ ] Other (please describe below)

---
+++ docs/astro.config.mjs
@@ -0,0 +1,33 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+import starlightThemeRapide from "starlight-theme-rapide";
+
+export default defineConfig({
+  integrations: [
+    starlight({
+      plugins: [starlightThemeRapide()],
+      title: "Rivu",
+      social: [
+        {
+          icon: "github",
+          label: "GitHub",
+          href: "https://github.com/Jarmos-san/rivu",
+        },
+      ],
+      sidebar: [
+        {
+          label: "Guides",
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: "Example Guide", slug: "guides/example" },
+          ],
+        },
+        {
+          label: "Reference",
+          autogenerate: { directory: "reference" },
+        },
+      ],
+    }),
+  ],
+});
